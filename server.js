a9e06ebccadccafa2f430c4c3df7214c62b71a44
Will Fritts <waf515@gmail.com>
2019-05-01 08:57:54 -0700

    copied in server code from previous lab

---
+++ server.js
@@ -0,0 +1,73 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.static('./public'));
+
+app.get('/', (request, response) => {
+  response.send('server works');
+});
+
+function GEOloc(query, fmtQ, lat, long) {
+  this.search_query = query;
+  this.formatted_query = fmtQ;
+  this.latitude = lat;
+  this.longitude = long;
+}
+
+function Forecast(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
+}
+
+function handleError() {
+  return { 'status': 500, 'responseText': 'Sorry, something went wrong' };
+}
+
+app.get('/location', (request, response) => {
+  try {
+    const data = require('./data/geo.json');
+    // console.log(data.address_component);
+    let city = new GEOloc(request.query.data, data.results[0].formatted_address, data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
+    response.send(city);
+  } catch (error) {
+    response.send(handleError);
+  }
+
+});
+
+
+app.get('/weather', (request, response) => {
+  try {
+    const data = require('./data/darksky.json');
+    let daily = Object.entries(data)[6];
+    let dailyData = daily[1].data;//hourly day forecast
+
+    let myForecast = [];
+    dailyData.forEach(element => {
+      let date = new Date(element.time * 1000).toDateString();
+      let temp = new Forecast(element.summary, date);
+      myForecast.push(temp);
+    });
+    console.log(myForecast);
+    response.send(myForecast);
+
+  } catch (error) {
+    response.send(handleError);
+  }
+
+
+
+});
+
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
